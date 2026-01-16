988dc873ccce87e95853ff00e04f7b1858de7885
Matthias Gielen <digiusepp3@users.noreply.github.com>
2026-01-16 17:23:00 +0000

    auto-commit for c4844443-1fe5-4d4a-b009-0ad16ef55f10

--- backend_test.py
+++ backend_test.py
@@ -6,7 +6,7 @@ import json
 from datetime import datetime
 
 class CodeForgeAPITester:
-    def __init__(self, base_url="https://appfix-dev.preview.emergentagent.com/api"):
+    def __init__(self, base_url="https://glassgpt-overhaul.preview.emergentagent.com/api"):
         self.base_url = base_url
         self.token = None
         self.tests_run = 0
